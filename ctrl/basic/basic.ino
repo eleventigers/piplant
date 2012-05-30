b7d136f7c08f24e4cc30b3a51f1c064805a5ba31
eleventigers <phonodelik@gmail.com>
2012-05-30 23:31:05 +0100

    non blocking sensor reading and pump ctrl

--- ctrl/basic/basic.ino
+++ ctrl/basic/basic.ino
@@ -7,9 +7,22 @@ const int tempSensPin = A1;
 
 const int pumpCtrlPin = 13;
 const int lightCtrlPin = 12;
+ 
+const int tempSmoothSteps = 10;
+const int tempSensRate = 1000;
+float tempSensRaw, tempSensCurr, tempSensHist[tempSmoothSteps];
 
-int moistCurrRead = 0;        
-int tempCurrRead = 0;   
+const int moistSmoothSteps = 10;
+const int moistSensRate = 2000;
+const int moistLow = 450;
+const int moistHigh = 850;
+float moistSensRaw, moistSensCurr, moistSensHist[moistSmoothSteps];
+
+const int pumpCycle = 10000;
+
+unsigned long currentTime, tempSensLast, moistSensLast, pumpOnStart;
+
+int pumpOn = 0;
 
 void setup() {
   
@@ -26,53 +39,90 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value:
-  moistCurrRead = analogRead(moistSensPin);
-  tempCurrRead = analogRead(tempSensPin);    
-
-//  if (sensorValue <= 450) {
-//    digitalWrite(13, HIGH); 
-//  }
-//  if (sensorValue > 850) {
-//    digitalWrite(13, LOW); 
-//  }
   
-  // print the results to the serial monitor:
-  Serial.print("moisture = " );                       
-  Serial.print(moistCurrRead);      
-  Serial.print("\t temperature = ");      
-  Serial.println(LM335ATempConvert(tempCurrRead, 'C'));   
+  currentTime = millis();
+  
+  if (abs(currentTime - tempSensLast) >= tempSensRate) {
+    tempRead();  
+  } 
+  
+  if (abs(currentTime - moistSensLast) >= moistSensRate && pumpOn != 1) {  
+    moistRead();  
+  } 
+  
+  if (moistSensCurr <= moistLow && pumpOn == 0){
+    pumpOnStart = currentTime;
+    digitalWrite(pumpCtrlPin, HIGH);
+    pumpOn = 1;
+    Serial.print("\t pump on");
+  }
+  
+  if (pumpOn == 1) {
+    if (abs(currentTime - pumpOnStart) >= pumpCycle){
+      if (abs(currentTime - moistSensLast) >= moistSensRate){  
+        moistRead();  
+      } 
+      if (moistSensCurr >= moistHigh){
+        digitalWrite(pumpCtrlPin, LOW);
+        pumpOn = 0;
+        Serial.print("\t pump off");   
+      }
+    } 
+  }
+  
+}
 
-  // wait 100 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(1000);        
+void moistRead(){
+  
+    moistSensLast = currentTime;
+    
+    moistSensRaw = analogRead(moistSensPin);
+    moistSensCurr = moistSensRaw;
+    for(int i=moistSmoothSteps-1; i>0; i--){
+      moistSensHist[i] = moistSensHist[i-1];
+      moistSensCurr += moistSensHist[i];
+    }
+    moistSensHist[0] = moistSensRaw;
+    moistSensCurr = moistSensCurr / moistSmoothSteps;  
+   
+    Serial.print("\t moisture = ");      
+    Serial.println(moistSensCurr); 
+  
+}
+
+void tempRead(){
+  
+    tempSensLast = currentTime;
+    
+    tempSensRaw = getTemp(tempSensPin);
+    tempSensCurr = tempSensRaw;
+    for(int i=tempSmoothSteps-1; i>0; i--){
+      tempSensHist[i] = tempSensHist[i-1];
+      tempSensCurr += tempSensHist[i];
+    }
+    tempSensHist[0] = tempSensRaw;
+    tempSensCurr = tempSensCurr / tempSmoothSteps;  
+   
+    Serial.print("\t temperature = ");      
+    Serial.println(tempSensCurr);   
+     
 }
 
 // >> temperature functions
 // ---------------------------
 
-// math learned from GreenRobotics in a comment on SparkFun's website
-int LM335ATempConvert(int tempIn, char unitSystem){
-  int KelvinC=273;
-  int KelvinTemp = (long(tempIn) * 5 * 100) / 1023; // convert 
-  int CelsiusTemp = KelvinTemp-KelvinC;
-  int FahrenheitTemp = (CelsiusTemp)*(9/5)+32;
-  int tempOut;
 
-  switch(unitSystem){
-  case 'K':
-    tempOut = KelvinTemp;
-    break;
-  case 'C':
-    tempOut = CelsiusTemp;
-    break;
-  case 'F':
-    tempOut = FahrenheitTemp;
-    break;
-  }
-  return tempOut;
+float getTemp(int tempPin){
+  
+  float temperature = getVoltage(tempPin);
+  temperature = (temperature - .5) * 100;
+  
+  return temperature;
 }
+
+float getVoltage(int pin){
+    return (analogRead(pin) * .004882814);
+  }
 
 // ---------------------------
 // << temperature functions
